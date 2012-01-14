2449d96980533154bfdc31245a6b33a95eb4ae3e
u16suzu <u16suzu@gmail.com>
2012-01-14 18:09:11 +0900

    打率計算のメソッドの三角測量のためのテストを追加

--- spec/batting_average_spec.rb
+++ spec/batting_average_spec.rb
@@ -3,9 +3,18 @@ require 'batting_average.rb'
 
 describe BattingAverage do
   describe "#calc" do
-    it "打率を計算できること" do
-      batting_average = BattingAverage.new
-      batting_average.calc(4, 4, 1).should == 0.25
+    context "4打席4打数1安打の時" do
+      it "打率が0.25になること" do
+        batting_average = BattingAverage.new
+        batting_average.calc(4, 4, 1).should == 0.25
+      end
+    end
+
+    context "4打席4打数2安打の時" do
+      it "打率が0.5になること" do
+        batting_average = BattingAverage.new
+        batting_average.calc(4, 4, 2).should == 0.5
+      end
     end
     
   end
