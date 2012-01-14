ffca21a7db2847d3ac91adfb8d1884ae1cabe4a5
u16suzu <u16suzu@gmail.com>
2012-01-14 18:44:09 +0900

    打率の整数部を消去して表示する処理を実装

--- lib/batting_average.rb
+++ lib/batting_average.rb
@@ -4,5 +4,9 @@ class BattingAverage
     return 0.0 if stroke_count == 0
     (hit_count.to_f / stroke_count.to_f).round 3
   end
+
+  def calc_to_s(turn, stroke_count, hit_count)
+    calc(turn, stroke_count, hit_count).to_s.sub(/^0/, "")
+  end
   
 end
