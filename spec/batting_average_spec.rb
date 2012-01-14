db80f6219c4ba5d38a000059e5e088bb72610619
u16suzu <u16suzu@gmail.com>
2012-01-14 18:41:31 +0900

    打率の整数部を消去して表示する事に関するテストを追加

--- spec/batting_average_spec.rb
+++ spec/batting_average_spec.rb
@@ -2,10 +2,10 @@
 require 'batting_average.rb'
 
 describe BattingAverage do
+  before do
+    @batting_average = BattingAverage.new
+  end
   describe "#calc" do
-    before do
-      @batting_average = BattingAverage.new
-    end
 
     context "4打席4打数1安打の時" do
       it "打率が0.25になること" do
@@ -35,6 +35,12 @@ describe BattingAverage do
       it "0.0と計算すること" do
         @batting_average.calc(1, 0, 0).should == 0.0
       end
+    end
+  end
+
+  describe "#calc_to_s" do
+    it "打率の整数部は消去して表示すること" do
+      @batting_average.calc_to_s(3, 3, 1).should == ".333"
     end
   end
 end
